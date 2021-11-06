aec8d85509600a32d1472e070497f9fd1ba5df9a
Tatiana Ryzhova <tryzhova93@gmail.com>
2021-11-07 00:10:25 +0100

    add data json

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -3,6 +3,8 @@ import personalPicture from "../../images/personal_picture.png"
 import cv from "../../files/tatiana_ryzhova_cv.pdf"
 import arrowPic from "../../images/arrow-down.gif"
 
+const data = require("../../data.json")
+
 function AboutMe() {
   return (
     <section className="about-me" id="about-me">
@@ -13,14 +15,9 @@ function AboutMe() {
         </div>
 
         <div className="about-me__information">
-          <p className="about-me__specialty">SOFTWARE ENGINEER</p>
-          <h1 className="about-me__name">Tatiana Ryzhova</h1>
-          <p className="about-me__description">I was born and live in Saratov, graduated from the Faculty of Economics at SSU.
-            I have a wife
-            and daughter. I love listening to music, and I'm also fond of running. Recently started coding. Since 2015,
-            he worked at SKB Kontur. After completing a course in web development, I started doing freelance orders and
-            left my permanent job.
-          </p>
+          <p className="about-me__specialty">{data.specialty}</p>
+          <h1 className="about-me__name">{data.name}</h1>
+          <p className="about-me__description">{data.description}</p>
 
           <div className="about-me__links">
             <a href={cv} className="about-me__link about-me__link_cv" target="_blank" rel="noreferrer">DOWNLOAD CV</a>
--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -5,6 +5,8 @@ import linkedinIcon from "../../images/linkedin_icon.svg"
 import arrowPic from "../../images/arrow.png"
 import telegramIcon from "../../images/telegram.png";
 
+const data = require("../../data.json")
+
 function ContactForm() {
   return (
     <section className="contact" id="contactForm">
@@ -17,40 +19,40 @@ function ContactForm() {
         >
           <div className="contact__name">
             <h4 className="contact__text_name">
-              Tatiana Ryzhova
+              {data.name}
             </h4>
-            <p className="contact__text_speciality">Software Engineer</p>
+            <p className="contact__text_speciality">{data.specialty}</p>
           </div>
           <ul>
             <li>
               <p className="contact__text">
                 <span>PHONE: </span>
-                +49 152 378 068 22
+                {data.phone}
               </p>
             </li>
             <li>
               <p className="contact__text">
                 <span>E-MAIL: </span>
-                tanyaryzhova93@gmail.com
+                {data.email}
               </p>
             </li>
             <li>
               <p className="contact__text">
                 <span>ADDRESS: </span>
-                Berlin, Germany
+                {data.address}
               </p>
             </li>
           </ul>
           <div className="contact__social-links">
-            <a href="https://www.linkedin.com/in/%F0%9F%96%87tatiana-ryzhova-36961b16a/" target="_blank"
+            <a href={data.linkedIn} target="_blank"
                className="contact__social-link">
               <img src={linkedinIcon} alt="linkedin icon" className="contact__social-icon"/>
             </a>
-            <a href="https://github.com/TatianaRyzhova" target="_blank"
+            <a href={data.github} target="_blank"
                className="contact__social-link">
               <img src={githubIcon} alt="github icon" className="contact__social-icon"/>
             </a>
-            <a href="https://t.me/tanya_tiny" target="_blank"
+            <a href={data.telegram} target="_blank"
                className="contact__social-link">
               <img src={telegramIcon} alt="telegram icon" className="contact__social-icon"/>
             </a>
